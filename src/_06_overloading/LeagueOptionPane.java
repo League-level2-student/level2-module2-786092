8f3711dd492362d8cae405c487b18f700a4a9761
786092 <Irfaan.S7860@gmail.com>
2025-02-23 12:56:07 -0800

    Overloading in Progress

--- src/_06_overloading/LeagueOptionPane.java
+++ src/_06_overloading/LeagueOptionPane.java
@@ -1,5 +1,6 @@
 package _06_overloading;
 
+import java.awt.Frame;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -20,10 +21,18 @@ public class LeagueOptionPane {
 	public static void showMessageDialog(String message) {
 		// 1. Open example.png and make a GUI that looks like that
 		//    The message parameter is what we want to show on our pop-up
-		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(loadImage("league.png"));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		panel.add(label);
+		frame.add(panel);
+		label.setText(message);
+		frame.pack();
 		
 		// 2. Uncomment the line of code below. It sets the location of our frame to the center of the screen
-		//frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null);
 	}
 	
 	// 3. Call this method in the Runner class
@@ -31,12 +40,38 @@ public class LeagueOptionPane {
 
 	// 4. Create another showMessageDialog() method that lets us also choose the Message and Title 
 	//    2 String parameters (one for the message and one for the title)
+	public static void showMessageDialog(String message, String title) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(loadImage("league.png"));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		panel.add(label);
+		frame.add(panel);
+		frame.setTitle(title);
+		label.setText(message);
+		frame.pack();
+		
+	}
 	
 	// 5. Call this method in the Runner class
 	
 	
 	// 6. Create another showMessageDialog() method that lets us choose the Message, Title, and Image
 	//    3 String parameters (one for the message, one for the title, and one for the fileName)
+	public static void showMessageDialog(String message, String title, String fileName) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(loadImage(fileName));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		panel.add(label);
+		frame.add(panel);
+		frame.setTitle(title);
+		label.setText(message);
+		frame.pack();
+		
+	}
 	
 	// 7. Call this method in the Runner class
 	
--- src/_06_overloading/LeagueOptionPaneRunner.java
+++ src/_06_overloading/LeagueOptionPaneRunner.java
@@ -2,6 +2,8 @@ package _06_overloading;
 
 public class LeagueOptionPaneRunner {
 	public static void main(String[] args) {
-		
+		LeagueOptionPane.showMessageDialog("Anybody can code");
+		LeagueOptionPane.showMessageDialog("Take a <break>", "The League");
+		LeagueOptionPane.showMessageDialog("Anybody can code", "How to code", "java.png");
 	}
 }
